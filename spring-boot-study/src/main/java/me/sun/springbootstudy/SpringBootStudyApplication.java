e6b51cb80eeffe93666b981f89af3c44b19b67c8
DongMyeongLee <dongmyeong.lee22@gmail.com>
2019-12-21 01:31:13 +0900

    Spring, Vue 프로젝트 생성 및 동작 확인

---
+++ spring-boot-study/src/main/java/me/sun/springbootstudy/SpringBootStudyApplication.java
@@ -0,0 +1,13 @@
+package me.sun.springbootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudyApplication.class, args);
+    }
+
+}
---
+++ spring-boot-study/src/test/java/me/sun/springbootstudy/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sun.springbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
